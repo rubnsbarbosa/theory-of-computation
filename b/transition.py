cd76d17714e7cf819be203a688955f6b2e5ea67c
rubnsbarbosa <rubensbarbosa90@gmail.com>
2020-09-21 11:31:17 -0300

    transition

--- b/transition.py
+++ b/transition.py
@@ -3,6 +3,3 @@ class Transition:
         self.origin = origin
         self.symbol = symbol
         self.destiny = destiny
-
-    def __str__(self):
-        return '\tS({},{}) => {}'.format(self.origin, self.symbol, self.destiny)
