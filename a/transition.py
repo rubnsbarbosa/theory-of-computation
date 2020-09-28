80f3990856f00842cdc8dbb75737bb6cbf52a588
rubnsbarbosa <rubensbarbosa90@gmail.com>
2020-09-28 13:47:34 -0300

    transition

---
+++ a/transition.py
@@ -0,0 +1,10 @@
+class Transition:
+    
+    def __init__(self, origin, symbol, destiny):
+        self.origin = origin
+        self.symbol = symbol
+        self.destiny = destiny
+
+    def __str__(self):
+        return "\tS({},{}) => {}".format(self.origin, self.symbol, self.destiny)
+        
